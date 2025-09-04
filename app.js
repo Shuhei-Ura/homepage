bfc1cbfa380bf2f8021883c896ec55837db93407
Shuhei Ura <syuuhei.ura1862@gmail.com>
2025-09-04 15:11:40 +0900

    Nodemailerにtls追加

--- app.js
+++ app.js
@@ -174,6 +174,9 @@ const mailer = nodemailer.createTransport({
   port: Number(process.env.SMTP_PORT),
   secure: Number(process.env.SMTP_PORT) === 587, // 465=SSL
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
+  tls: {
+    rejectUnauthorized: false // Gmailなら基本OK
+  }
 });
 
 // --- 3) 通知メール送信のユーティリティ ---
